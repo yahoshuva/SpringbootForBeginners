f0fb580809f4ae955708c95fafd0da8c18a9b294
Yahoshuva <pasupuletiyahoshuva1999@gmail.com>
2022-07-06 14:35:16 -0700

    first commit

---
+++ src/main/java/com/example/demo/SpringBootForBeginnersApplication.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootForBeginnersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootForBeginnersApplication.class, args);
+		System.out.println("App run");
+	}
+
+}
